38006d3461a480af96b600fa638236dc392539af
ReguTogu <clone1cuato@gmail.com>
2024-12-11 15:36:57 +0700

    New Test

--- QuanLyKinhDoanhVangBacDaQuy/QuanLyKinhDoanhVangBacDaQuy/obj/Release/net8.0-windows/QuanLyKinhDoanhVangBacDaQuy.AssemblyInfo.cs
+++ QuanLyKinhDoanhVangBacDaQuy/QuanLyKinhDoanhVangBacDaQuy/obj/Release/net8.0-windows/QuanLyKinhDoanhVangBacDaQuy.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyKinhDoanhVangBacDaQuy")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcaef40545f4a25d9401d0d364b07bf22e60aec1")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyKinhDoanhVangBacDaQuy")]
